0e87d8c3b5cf56e8621f22ec90613617e9ae1d88
bowski43 <43mngt@gmail.com>
2024-01-25 11:37:18 +0300

    Initial Commit

---
+++ RandomAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomAnimation
+//
+//  Created by Serge Bowski on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
