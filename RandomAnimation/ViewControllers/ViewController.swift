c16239d53332f7db58e98b37cc8626eadfb5f6ce
bowski43 <43mngt@gmail.com>
2024-01-26 14:46:58 +0300

    fix issues

--- RandomAnimation/ViewControllers/ViewController.swift
+++ RandomAnimation/ViewControllers/ViewController.swift
@@ -27,31 +27,34 @@ final class ViewController: UIViewController {
         durationLabel.text = "Duration: \(springView.duration)"
         delayLabel.text = "Delay: \(springView.delay)"
     }
+    
+    private var firstEntry = true
 
     @IBAction private func animationButtonPressed(_ sender: UIButton) {
-        springView.animate()
-        
-        springView.animation = AnimationPreset.allCases.randomElement()?.rawValue ?? ""
-        springView.curve = AnimationCurve.allCases.randomElement()?.rawValue ?? ""
-        sender.setTitle("Run \(springView.animation)", for: .normal)
-        
-        springView.force = CGFloat.random(in: (1.0...2.0))
-        springView.duration = CGFloat.random(in: (0.1...1.5))
-        springView.delay = CGFloat.random(in: (0.0...1.0))
-        
-        springView.animate()
-        
-        presetLabel.text = "Preset: \(springView.animation)"
-        curveLabel.text = "Curve: \(springView.curve)"
-        forceLabel.text = String(format: "Force: %0.2f", springView.force)
-        durationLabel.text = String(format: "Duration: %0.2f", springView.duration)
-        delayLabel.text = String(format: "Delay: %0.2f", springView.delay)
-        
-        
-
-        
-        
+        if firstEntry {
+            springView.animate()
+            firstEntry.toggle()
+            
+            springView.animation = AnimationPreset.allCases.randomElement()?.rawValue ?? ""
+            sender.setTitle("Run \(springView.animation)", for: .normal)
+        } else {
+            springView.animation = sender.titleLabel?.text?.deletingPrefix("Run ") ?? ""
+            springView.curve = AnimationCurve.allCases.randomElement()?.rawValue ?? ""
+               
+            springView.force = CGFloat.random(in: (1.0...2.0))
+            springView.duration = CGFloat.random(in: (0.1...1.5))
+            springView.delay = CGFloat.random(in: (0.0...1.0))
+            
+            presetLabel.text = "Preset: \(springView.animation)"
+            curveLabel.text = "Curve: \(springView.curve)"
+            forceLabel.text = String(format: "Force: %0.2f", springView.force)
+            durationLabel.text = String(format: "Duration: %0.2f", springView.duration)
+            delayLabel.text = String(format: "Delay: %0.2f", springView.delay)
+            
+            springView.animate()
+            
+            springView.animation = AnimationPreset.allCases.randomElement()?.rawValue ?? ""
+            sender.setTitle("Run \(springView.animation)", for: .normal)
+        }
     }
 }
-
-
