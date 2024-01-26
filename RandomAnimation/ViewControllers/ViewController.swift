8fdd860620f2d9bb56f9c4843d206586ea74ca70
bowski43 <43mngt@gmail.com>
2024-01-26 08:36:14 +0300

    setup ui

--- RandomAnimation/ViewControllers/ViewController.swift
+++ RandomAnimation/ViewControllers/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func animateButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
